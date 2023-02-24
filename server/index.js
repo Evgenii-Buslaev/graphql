d5fc810e56a0956fa453cd638002be86473d2800
EvgeniiBuslaev <eugenebus144@gmail.com>
2023-02-24 16:38:41 +0500

    Created schema for data

--- server/index.js
+++ server/index.js
@@ -1,13 +1,51 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
+const schema = require("./schema.js");
+const users = [
+  {
+    id: 1,
+    username: "Eugene",
+    age: 27,
+  },
+  {
+    id: 2,
+    username: "Irina",
+    age: 25,
+  },
+];
 
 const app = express();
 app.use(cors());
+
+const createUser = (input) => {
+  const id = Date.now();
+  return {
+    id,
+    ...input,
+  };
+};
+
+const root = {
+  getAllUsers: () => {
+    return users;
+  },
+  getUser: ({ id }) => {
+    return users.find((user) => user.id == id);
+  },
+  createUser: ({ input }) => {
+    const user = createUser(input);
+    users.push(user);
+    return user;
+  },
+};
+
 app.use(
   "/graphql",
   graphqlHTTP({
     graphiql: true,
+    schema,
+    rootValue: root,
   })
 );
 
